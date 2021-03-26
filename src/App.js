4793dde94b3f7112c15fdbe9f161824d04818ff7
artur <arturkabirov16@gmail.com>
2021-03-26 18:36:48 +0600

    Deploy applicants page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import MyTextField from "./Components/form/TextField";
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import ForApplicants from "./Components/Pages/ForApplicants";
+import {
+	BrowserRouter as Router,
+	Route,
+} from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="App">
-     <ForApplicants />
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<div className="App">
+					<Route exact path={"/local_elections/applicants/"} component={ForApplicants}/>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
