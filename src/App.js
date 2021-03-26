4a2dbf51dbf6e203f7b533fb87bcf80738a54138
artur <arturkabirov16@gmail.com>
2021-03-26 18:45:53 +0600

    Try to fix bugs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 		<div>
 			<Router>
 				<div className="App">
-					<Route exact path={"/local_elections/applicants/"} component={ForApplicants}/>
+					<Route exact path={"/local_elections"} component={ForApplicants}/>
 				</div>
 			</Router>
 		</div>
