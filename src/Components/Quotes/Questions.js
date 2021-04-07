ed778f76fad80ec34e79a73cabddb70191e6ac45
artur <arturkabirov16@gmail.com>
2021-04-07 20:15:53 +0600

    fdfd

--- src/Components/Quotes/Questions.js
+++ src/Components/Quotes/Questions.js
@@ -92,7 +92,7 @@ const Questions = (props) => {
 		let curr_questions = (lang == "ru") ? "all_questions" : "all_questions_kg";
 		db.collection("questions").doc(curr_questions)
 			.onSnapshot((doc) => {
-				setQuestions(doc.data().questions.slice(0,2));
+				setQuestions(doc.data().questions);
 			});
 
 		let curr_axises = (lang == "ru") ? "axises" : "axises_kg";
